e739d92eb42790ff84bf8ddf1dc0b56ede20cf22
VNKPsych <tobytechs101@gmail.com>
2021-12-28 15:41:40 +0000

    Circle Update

--- frontend/src/Circle.js
+++ frontend/src/Circle.js
@@ -1,9 +1,11 @@
 import CircleImage from './CircleStructure.png'
+import CircleMeeting from './CircleMeeting.png'
 const Circle = () => {
     return ( 
         <div className="circle">
             <h2>Members of Circle</h2>
             <img src = {CircleImage} className = "circleimage" alt="circle members" />
+            <img src = {CircleMeeting} className="circleimage" alt="circle members together" />
         </div>
      );
 }
